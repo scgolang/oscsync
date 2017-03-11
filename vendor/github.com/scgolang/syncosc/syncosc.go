5ca74f8794ca6befc7a8e89b5931e10c33190878
Brian Sorahan <bsorahan@gmail.com>
2017-03-11 09:07:47 -0600

    add vendor

---
+++ vendor/github.com/scgolang/syncosc/syncosc.go
@@ -0,0 +1,11 @@
+// Package syncosc exists to define constants used in the oscsync protocol.
+// See http://github.com/scgolang/oscsync/README.md
+package syncosc
+
+const (
+	AddressPulse       = "/sync/pulse"
+	AddressSlaveAdd    = "/sync/slave/add"
+	AddressSlaveList   = "/sync/slave/list"
+	AddressSlaveRemove = "/sync/slave/remove"
+	AddressTempo       = "/sync/tempo"
+)
