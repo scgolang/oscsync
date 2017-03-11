15a134e924c1f2f8e3052eed8099d681eb635852
Brian Sorahan <bsorahan@gmail.com>
2017-03-11 14:35:43 -0600

    update vendor

---
+++ vendor/github.com/scgolang/syncclient/syncclient.go
@@ -0,0 +1,124 @@
+// Package syncclient allows programs to easily sync to an oscsync master.
+package syncclient
+
+import (
+	"context"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/scgolang/osc"
+	"github.com/scgolang/syncosc"
+	"golang.org/x/sync/errgroup"
+)
+
+// Slave is any type that can sync to an oscsync master.
+// The slave's Pulse method will be invoked every time a new pulse is received
+// from the oscsync master.
+type Slave interface {
+	Pulse(syncosc.Pulse) error
+}
+
+// Connect connects a slave to an oscsync master.
+// This func blocks forever.
+func Connect(ctx context.Context, slave Slave, host string) error {
+	local, err := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+	if err != nil {
+		return errors.Wrap(err, "creating listening address")
+	}
+	remote, err := net.ResolveUDPAddr("udp", net.JoinHostPort(host, strconv.Itoa(syncosc.MasterPort)))
+	if err != nil {
+		return errors.Wrap(err, "creating listening address")
+	}
+	g, gctx := errgroup.WithContext(ctx)
+
+	conn, err := osc.DialUDPContext(gctx, "udp", local, remote)
+	if err != nil {
+		return errors.Wrap(err, "connecting to master")
+	}
+	pulseChan := make(chan syncosc.Pulse, 1)
+
+	// Start the OSC server so we receive the master's messages.
+	g.Go(func() error {
+		return tickerLoop(gctx, slave, pulseChan)
+	})
+	g.Go(func() error {
+		return receivePulses(conn, pulseChan)
+	})
+	// Announce the slave to the master.
+	portStr := strings.Split(conn.LocalAddr().String(), ":")[1]
+	lport, err := strconv.ParseInt(portStr, 10, 64)
+	if err != nil {
+		return errors.Wrapf(err, "parsing int from %s", portStr)
+	}
+	if err := conn.Send(osc.Message{
+		Address: syncosc.AddressSlaveAdd,
+		Arguments: osc.Arguments{
+			osc.String("127.0.0.1"),
+			osc.Int(lport),
+		},
+	}); err != nil {
+		return errors.Wrap(err, "sending add-slave message")
+	}
+	return nil
+}
+
+func receivePulses(conn osc.Conn, pulseChan chan<- syncosc.Pulse) error {
+	return conn.Serve(osc.Dispatcher{
+		syncosc.AddressPulse: osc.Method(func(m osc.Message) error {
+			pulse, err := syncosc.PulseFromMessage(m)
+			if err != nil {
+				return errors.Wrap(err, "getting pulse from message")
+			}
+			pulseChan <- pulse
+			return nil
+		}),
+	})
+}
+
+func tickerLoop(ctx context.Context, slave Slave, pulseChan <-chan syncosc.Pulse) error {
+	var (
+		p      syncosc.Pulse
+		ticker *time.Ticker
+	)
+
+	// Don't do anything until we have received the first pulse.
+	// The master should send this right after the announcement is successful.
+	// Announcement shouldn't take any longer than a second.
+	// In fact, it shouldn't take longer than a couple milliseconds.
+ExpectPulse:
+	select {
+	case <-ctx.Done():
+		return nil
+	case <-time.After(1 * time.Second):
+		return errors.New("timeout waiting for first pulse")
+	case p = <-pulseChan:
+	}
+
+	ticker = time.NewTicker(syncosc.GetPulseDuration(p.Tempo))
+
+EnterLoop:
+	for range ticker.C {
+		// Has there been a tempo update?
+		select {
+		default:
+			p.Count++
+			if err := slave.Pulse(p); err != nil {
+				return errors.Wrap(err, "invoking slave.Pulse")
+			}
+			if p.Count%(syncosc.PulsesPerBar-1) == 0 {
+				// We are about to hit a new bar.
+				goto ExpectPulse
+			}
+		case <-ctx.Done():
+			return nil
+		case p = <-pulseChan:
+			// Tempo has changed.
+			ticker = time.NewTicker(syncosc.GetPulseDuration(p.Tempo))
+			goto EnterLoop
+		}
+	}
+	return nil
+}
--- vendor/github.com/scgolang/syncosc/syncosc.go
+++ vendor/github.com/scgolang/syncosc/syncosc.go
@@ -2,6 +2,13 @@
 // See http://github.com/scgolang/oscsync/README.md
 package syncosc
 
+import (
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/scgolang/osc"
+)
+
 // OSC addresses.
 const (
 	AddressPulse       = "/sync/pulse"
@@ -16,3 +23,37 @@ const MasterPort = 5776
 
 // PulsesPerBar is the number of pulses in a bar (measure).
 const PulsesPerBar = 96
+
+// GetPulseDuration converts the tempo in bpm to a time.Duration
+// callers are responsible for making concurrent access safe.
+func GetPulseDuration(tempo float32) time.Duration {
+	if tempo == 0 {
+		return time.Duration(0)
+	}
+	return time.Duration(float32(int64(24e10)/PulsesPerBar) / tempo)
+}
+
+// Pulse represents the arguments in a /sync/pulse message.
+type Pulse struct {
+	Tempo float32
+	Count int32
+}
+
+// PulseFromMessage gets a Pulse from an OSC message.
+func PulseFromMessage(m osc.Message) (Pulse, error) {
+	p := Pulse{}
+	if expected, got := 2, len(m.Arguments); expected != got {
+		return p, errors.Errorf("expected %d arguments, got %d", expected, got)
+	}
+	tempo, err := m.Arguments[0].ReadFloat32()
+	if err != nil {
+		return p, errors.Wrap(err, "reading tempo")
+	}
+	count, err := m.Arguments[0].ReadInt32()
+	if err != nil {
+		return p, errors.Wrap(err, "reading counter")
+	}
+	p.Tempo = tempo
+	p.Count = count
+	return p, nil
+}
